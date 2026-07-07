5e38a1709b1c47b46e9101f05125bed0817089d5
Anuj Naruka <anujnaruka28@gmail.com>
2026-07-07 17:50:22 +0530

    issue: react .d.ts files issue

--- types/react.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react' {
-    const React: any;
-    export default React;
-}
-
-declare module 'react-dom/server' {
-    export const renderToString: any;
-    export default any;
-}
